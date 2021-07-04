6b014d783abccd8ed1da5c3c03bb546cfebfb64f
kerby <lovincekerby99@gmail.com>
2021-07-04 00:33:37 -0300

    commit

--- Circulo.java
+++ Circulo.java
@@ -1,16 +1,49 @@
-class Circulo {
+  public  class Circulo {
     private int raio;
-    int Raio;
-    double area;
-    double circunf;
+    Circulo (int raio){};
+    double area ;
+    double circunf ;
+    
 
-    public Circulo(int raio, double area, double circunf){
-        this.raio=raio;
-        this.area=area;
-        this.circunf=circunf;
+    public Circulo(int raio,double area, double circunf){
+        setRaio(raio);
+        setArea(area);
+        setCircunf(circunf);
+        
+    }
+    public int getRaio() {
+        return this.raio;
     }
 
+    public void setRaio(int raio) {
+
+        this.raio = raio;
+    }
+
+    public double getArea() {
+        return this.area;
+    }
+
+    public void setArea(double area) {
+
+        this.area = area;
+    }
+
+    public double getCircunf() {
+        return this.circunf;
+    }
+
+    public void setCircunf(double circunf) {
+
+        this.circunf =circunf;
+    }
+
+
+
+
     void imprime() {
+        System.out.println("area = " + area);
+
         System.out.println("raio = " + raio); // imprimindo os atributos
         System.out.println("area = " + area);
         System.out.println("circunf = " + circunf);
--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args){
-        
+        Circulo c1,c2;
+        double raio=7;
+        c1 = new Circulo();
+        c1.area=4;
+        c1.circunf=10;
+
+
     }
     
 }
