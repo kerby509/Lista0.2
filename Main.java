39540db5f65beb93b47c64c983fbabbb30900288
kerby <lovincekerby99@gmail.com>
2021-07-04 14:00:58 -0300

    commit

--- Main.java
+++ Main.java
@@ -3,11 +3,13 @@ public class Main {
         
         Circulo c1 = new Circulo( 22,7.00,8.00);
         
-        //System.out.println("raio = " + c1.raio);
 
-        System.out.println("raio = " + c1.area);
-        System.out.println("area = " + c1.area);
-        System.out.println("circunf = " + c1.circunf);
+        System.out.println("O raio eh : " + c1.getRaio());
+        System.out.println("A area eh : " + c1.area);
+        System.out.println("A circunf eh : " + c1.circunf);
+// aqui tentando dar um valor que não pode inferior ao valor principal do raio
+        c1.setRaio(21);
+        System.out.println("Raio: " + c1.getRaio());
 
 
     }
