541e92e1b561e8e6b7d0f23040e29d364e3c57f4
kerby <lovincekerby99@gmail.com>
2021-08-21 13:45:25 -0300

    commit

---
+++ Lista1/Entidade.java
@@ -0,0 +1,23 @@
+package Lista1;
+
+public class Entidade {
+    private int id;
+    private Posicao posicao ;
+
+
+    public int getId(){
+        return this.id;
+    }
+
+    public void setId(int i){
+        this.id=i;
+    }
+    
+    public posicao getPosicao(){
+        return this.posicao;
+    }
+
+    public void setPosicao(posicao p){
+        this.posicao=p;
+    }
+}
