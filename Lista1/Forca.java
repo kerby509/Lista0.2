92829ccd910b87f8aacdfdec41ca7a9c63cf7f36
kerby <lovincekerby99@gmail.com>
2021-08-21 14:34:21 -0300

    commit

---
+++ Lista1/Forca.java
@@ -0,0 +1,27 @@
+package Lista1;
+
+public class Forca {
+    private int a;
+    private int b;
+
+
+    public int getA(){
+        return this.a;
+    }
+    
+    public void setA(int a){
+        this.a = a;
+    }
+
+    public int getB(){
+        return this.b;
+    }
+
+    public void setB(int b){
+        this.b = b;
+    }
+
+    public double tamanho(){
+        return this.a * this.b;
+    }
+}
