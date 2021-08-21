27d13a85d9eaa55271946cff491ff5e14a2428b1
kerby <lovincekerby99@gmail.com>
2021-08-21 14:58:21 -0300

    commit

---
+++ Lista1/Inimigo.java
@@ -0,0 +1,14 @@
+package Lista1;
+
+public class Inimigo {
+  
+    private int vida;
+
+    public int getVida(){
+        return this.vida;
+    }
+
+    public void setVida(int v){
+        this.vida = v;
+    }
+}
