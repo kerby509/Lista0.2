bd50b6697cb231c64e687185981ea2a468edcb56
kerby <lovincekerby99@gmail.com>
2021-09-21 23:34:22 -0300

    commit

--- Dinamica_2/Codigo-alpha/File.java
+++ Dinamica_2/Codigo-alpha/File.java
@@ -25,5 +25,72 @@ public class File {
         this.caminho = caminho;
         this.linhas = linhas;
     }
+
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCaminho() {
+        return this.caminho;
+    }
+
+    public void setCaminho(String caminho) {
+        this.caminho = caminho;
+    }
+
+    public String[] getLinhas() {
+        return this.linhas;
+    }
+
+    public String getLinhaPeloIndice(int indice) {
+        if(ehIndiceValido(indice) == false) {
+            return null;
+        }
+
+        return this.linhas[indice];
+    }
+
+    public int quantasLinhas() {
+        return this.linhas.length;
+    }
+
+    public void setLinhas(String[] linhas) {
+        this.linhas = linhas;
+    }
+
+    public boolean setLinha(int indice, String valor) {
+        if(ehIndiceValido(indice) == false) {
+            return false;
+        }
+
+        this.linhas[indice] = valor;
+        return true;
+    }
+
+    /**
+     * Retorna uma representação em texto do objeto arquivo. A representação é
+     * no formato "{nome='XYZ', caminho='XYZ', linhas=DD}", por exemplo:
+     * 
+     *   {nome='teste.txt', caminho='/home/fulano/', linhas=30}
+     * 
+     * Você pode usar esse método implicitamente quanto imprimir um objeto arquivo.
+     * Ex.:
+     *   Arquivo a = new Arquivo();
+     *   System.out.println(a); // imprime {nome='?', caminho='?', linhas=1}
+     */
+    public String toString() {
+        return "{" +
+            " nome='" + getNome() + "'" +
+            ", caminho='" + getCaminho() + "'" +
+            ", linhas='" + getLinhas().length + "'" +
+            "}";
+    }
+
+
     
 }
