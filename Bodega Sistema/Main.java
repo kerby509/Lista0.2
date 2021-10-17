aa69ac63c389fc62ffbbdc2b74393b35bc04d05c
kerby <lovincekerby99@gmail.com>
2021-10-17 13:15:56 -0300

    commit

--- Bodega Sistema/Main.java
+++ Bodega Sistema/Main.java
@@ -1,9 +1,26 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         
          int cliente=0;
          int bebida=0;
          int cod;
+
+
+    
+         Scanner entrada = new Scanner(System.in);
+
+        List<Clientes> cachaceiros =new ArrayList<>();
+        List<Bebida> bebidas =new ArrayList<>();
+
+        
+    
+       Bodega empres=new Bodega("Joãozin da Bogeda","JDF000555879");
+        System.out.println("--------------------------------------");
+        System.out.println("Nome da mpresa: "+empres.nomeDaEmpresa);
+        System.out.println("CNPJ Da Empresa:"+empres.cnpjDaEmpresa);
+       
    
    
    
