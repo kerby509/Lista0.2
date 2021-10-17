de08338e7d9a669ba5de52f40be0a2b46ae7322f
kerby <lovincekerby99@gmail.com>
2021-10-17 12:38:47 -0300

    commit

--- Bodega Sistema/Clientes.java
+++ Bodega Sistema/Clientes.java
@@ -3,6 +3,18 @@ public class Clientes {
     String nome;
     int cpf;
     boolean venderFiado;
+
+
+
+    Clientes(int codigo,String nome,int cpf,boolean venderFiado){
+        this.codigo=codigo;
+        this.nome=nome;
+        this.cpf=cpf;
+        this.venderFiado=venderFiado;
+
+
+
+}
     
     
 
